76b9fa07349588bf530bf0db39ef87a5edb9ddbd
asari <32879431+deomorxsy@users.noreply.github.com>
2022-10-31 08:55:08 -0300

    add replication script for connection with client daemons

---
+++ app/replication.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+from socket import *
+import json
+
+def main():
+
+    daemonPort = 8084
+
+    with open('../var/dnsTable.json', 'r') as f:
+        clients = f.load(f)
+        randomAddr = random.sample(sorted(clients), 1)
+        hostAddr = clients['randomAddr']
+        f.close()
+
+    #Create TCP/IP socket
+    repSock = socket(AF_INET, SOCK_STREAM)
+
+    #Connect socket to the same port as daemon (client daemon on port 8084)
+    repSock.connect((hostAddr, daemonPort))
+
+    # Send message to daemon listening
+    serverSend = '/// > Server says on {}: Spawn new server'.format(daemonPort)
+    repSock.send(serverSend.encode())
+    print(serverSend)
+
+    # Receive message if succeded
+    daemonRecv = repSock.recv(1024)
+    print('Daemon says on {}: {}'.format(daemonPort, daemonRecv.decode()))
+
+
+
+if __name__ == '__main__':
+    main()
